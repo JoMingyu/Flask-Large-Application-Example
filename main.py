b253df231dd9fa42cf92a8443adb6baa4dc57fbe
JoMingyu <city7310@naver.com>
2017-05-11 22:05:26 +0900

    db 모듈 작성
    MySQL 대응

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_restful import Api
+from support import database
 
 app = Flask(__name__)
 api = Api(app)
+
+db = database.Database()
+
 
 if '__main__' == __name__:
     app.run(host='0.0.0.0', port=80)
---
+++ support/database.py
@@ -0,0 +1,41 @@
+import pymysql
+from pymysql import IntegrityError
+
+
+class Database:
+    host = 'localhost'
+    user = 'root'
+    password = 'uursty199'
+    db = 'test'
+    charset = 'utf8'
+
+    _instance = None
+
+    def __init__(self):
+        self.connection = None
+
+    def execute(self, *args):
+        self.connection = pymysql.connect(host=self.host,
+                                          user=self.user,
+                                          password=self.password,
+                                          db=self.db,
+                                          charset=self.charset)
+        cursor = self.connection.cursor(pymysql.cursors.DictCursor)
+        query = ''
+        for arg in args:
+            query += arg
+
+        try:
+            if 'SELECT' in query or 'select' in query:
+                cursor.execute(query)
+                result = cursor.fetchall()
+            else:
+                cursor.execute(query)
+                result = self.connection.commit()
+
+            cursor.close()
+            self.connection.close()
+
+            return result
+        except IntegrityError as e:
+            print(e)
