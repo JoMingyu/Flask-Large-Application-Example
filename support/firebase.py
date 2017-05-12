a2f79ecfecee4cb611ca3b9314c496cdb798bb38
JoMingyu <city7310@naver.com>
2017-05-12 11:34:33 +0900

    빠트린 커밋

--- support/firebase.py
+++ support/firebase.py
@@ -9,3 +9,19 @@ def notify_single_device(message_title, message_body, registration_id):
                                                registration_id=registration_id)
 
     return result
+
+
+def notify_multiple_devices(message_title, message_body, registration_ids):
+    result = push_service.notify_multiple_devices(message_title=message_title,
+                                                  message_body=message_body,
+                                                  registration_ids=registration_ids)
+
+    return result
+
+
+def notify_topic_subscribers(message_title, message_body, topic_name):
+    result = push_service.notify_topic_subscribers(message_title=message_title,
+                                                   message_body=message_body,
+                                                   topic_name=topic_name)
+
+    return result
