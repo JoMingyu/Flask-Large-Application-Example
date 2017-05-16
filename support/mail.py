61ff520326ec912ad9843bd9fc0d2753272b0dd2
JoMingyu <city7310@naver.com>
2017-05-16 10:41:08 +0900

    이메일 전송 모듈 작성

---
+++ support/mail.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+
+from_email = ''
+smtp_host = 'smtp.naver.com'
+smtp_port = 587
+smtp_id = ''
+smtp_pw = ''
+
+
+def send(to_email, title, content):
+    message = MIMEText(content, _charset='utf-8')
+    message['Subject'] = title
+    message['From'] = from_email
+    message['To'] = to_email
+
+    s = smtplib.SMTP(smtp_host, smtp_port)
+    s.starttls()
+    s.login(smtp_id, smtp_pw)
+    s.sendmail(from_email, to_email, message.as_string())
+    s.close()
