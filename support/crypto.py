af1d631f76fa8f72c16dbb973dbe68dc362ab979
JoMingyu <city7310@naver.com>
2017-05-16 09:46:18 +0900

    파라미터 네임 변경

--- support/crypto.py
+++ support/crypto.py
@@ -2,15 +2,15 @@ import base64
 import hashlib
 
 
-def base64_encode(str):
-    return base64.b64encode(bytes(str, 'utf-8')).decode('utf-8')
+def base64_encode(s):
+    return base64.b64encode(bytes(s, 'utf-8')).decode('utf-8')
 
 
-def base64_decode(str):
-    return base64.b64decode(bytes(str, 'utf-8')).decode('utf-8')
+def base64_decode(s):
+    return base64.b64decode(bytes(s, 'utf-8')).decode('utf-8')
 
 
-def sha512_encrypt(str):
+def sha512_encrypt(s):
     h = hashlib.sha512()
-    h.update(str.encode('utf-8'))
+    h.update(s.encode('utf-8'))
     return h.hexdigest()
