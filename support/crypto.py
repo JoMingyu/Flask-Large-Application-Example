2c1d1acc2ccf54b516a83f813fce5ae451a9d7a8
JoMingyu <city7310@naver.com>
2017-05-16 09:45:44 +0900

    sha512 구현

--- support/crypto.py
+++ support/crypto.py
@@ -1,4 +1,5 @@
 import base64
+import hashlib
 
 
 def base64_encode(str):
@@ -8,3 +9,8 @@ def base64_encode(str):
 def base64_decode(str):
     return base64.b64decode(bytes(str, 'utf-8')).decode('utf-8')
 
+
+def sha512_encrypt(str):
+    h = hashlib.sha512()
+    h.update(str.encode('utf-8'))
+    return h.hexdigest()
