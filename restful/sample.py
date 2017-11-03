66d252c9e164eaa1ec7cda5d3db4d8bfed387bf2
JoMingyu <city7310@naver.com>
2017-11-04 01:24:50 +0900

    [REMOVE] Initialize

--- restful/sample.py
+++
@@ -1,9 +0,0 @@
-from flask import request
-from flask_restful import Resource
-
-
-class Sample(Resource):
-    def post(self):
-        data = request.form['test_form']
-
-        return '', 201
--- support/mail.py
+++ support/mail.py
@@ -1,23 +1,33 @@
 # -*- coding: utf-8 -*-
 
-import smtplib
+from smtplib import SMTP
 from email.mime.text import MIMEText
 
-from_email = ''
-smtp_host = ''
-smtp_port = 0
-smtp_id = ''
-smtp_pw = ''
 
+class Mail:
+    def __init__(self, smtp_url, smtp_port):
+        """
+        :type smtp_url: str
+        :type smtp_port: int
+        """
+        self.smtp = SMTP(smtp_url, smtp_port)
+        # Refer Config
+        self.smtp_id = ''
 
-def send(to_email, title, content):
-    message = MIMEText(content, _charset='utf-8')
-    message['Subject'] = title
-    message['From'] = from_email
-    message['To'] = to_email
+        self.smtp.starttls()
 
-    s = smtplib.SMTP(smtp_host, smtp_port)
-    s.starttls()
-    s.login(smtp_id, smtp_pw)
-    s.sendmail(from_email, to_email, message.as_string())
-    s.close()
+        # Refer Config : smtp email, smtp pw
+        # self.smtp.login('', '')
+
+    def send(self, subject, content, receiver_email):
+        msg = MIMEText(content, _charset='utf-8')
+
+        msg['subject'] = subject
+        # Refer Config
+        msg['from'] = self.smtp_id
+        msg['to'] = receiver_email
+
+        self.smtp.sendmail('', receiver_email, msg.as_string())
+
+    def __del__(self):
+        self.smtp.quit()
