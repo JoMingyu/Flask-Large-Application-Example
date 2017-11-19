bfab4225afea60abd702ea966a4a91e19262eefe
PlanB <city7310@naver.com>
2017-11-19 23:25:11 +0900

    [UPDATE] logger to middleware

--- Server/middleware.py
+++ Server/middleware.py
@@ -1,7 +1,7 @@
 from logging import INFO, Formatter
 from logging.handlers import RotatingFileHandler
 
-from flask import Flask, current_app, request
+from flask import current_app, request
 
 
 class Logger(object):
@@ -10,10 +10,7 @@ class Logger(object):
             self.init_app(app)
 
     def init_app(self, app):
-        """
-        :type app: Flask
-        :rtype: None
-        """
+        @app.before_first_request
         def make_logger():
             handler = RotatingFileHandler('server_log.log', maxBytes=100000, backupCount=5)
             handler.setFormatter(Formatter("[%(asctime)s] %(levelname)s - %(message)s"))
@@ -23,24 +20,21 @@ class Logger(object):
 
             current_app.logger.info('------ Logger Initialized ------')
 
-        def log_before_request():
+        @app.before_request
+        def before_request():
             current_app.logger.info('Requested from {0} [ {1} {2} ]'.format(request.host, request.method, request.url))
             current_app.logger.info('Request values : {0}'.format(request.values))
 
-        def log_after_request(response):
+        @app.after_request
+        def after_request(response):
             current_app.logger.info('Respond : {0}'.format(response.status))
 
-            response.headers['X-Powered-By'] = 'PlanB'
             response.headers['Content-Type'] = 'application/json; charset=utf8'
             # Fix encoding problem
 
             return response
 
-        def log_teardown_appcontext(exception):
+        @app.teardown_appcontext
+        def teardown_appcontext(exception):
             if not exception:
                 current_app.logger.info('Teardown appcontext successfully.')
-
-        app.before_first_request(make_logger)
-        app.before_request(log_before_request)
-        app.after_request(log_after_request)
-        app.teardown_appcontext(log_teardown_appcontext)
