a432718f3591d2fc89acb702a7f457f99a4729a2
PlanB <city7310@naver.com>
2017-11-19 23:24:57 +0900

    [DONE] ADD docs/, Mongo class

---
+++ Server/app/models/__init__.py
@@ -0,0 +1,18 @@
+from mongoengine import *
+
+
+class Mongo(object):
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        settings = app.config['MONGODB_SETTINGS']
+
+        connect(
+            alias=settings.get('db'),
+            host=settings.get('host'),
+            port=settings.get('port'),
+            username=settings.get('username'),
+            password=settings.get('password')
+        )
