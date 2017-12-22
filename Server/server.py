88670c95654babb4ecbe178a5ad628b67b53dc9c
PlanB <city7310@naver.com>
2017-12-22 11:44:09 +0900

    [UPDATE] threaded=True

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,4 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.debug)
+    app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.debug, threaded=True)
