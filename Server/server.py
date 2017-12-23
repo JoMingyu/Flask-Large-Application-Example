324fb04b639fac7416b4ba1562ca4fbd2a7e9849
JoMingyu <city7310@naver.com>
2017-12-23 19:01:52 +0900

    [UPDATE] Testcase autorun using unittest2

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,10 @@
+import unittest2 as ut
+
 from app import app
 
 if __name__ == '__main__':
+    if app.config['TEST']:
+        all_tests = ut.TestLoader().discover('tests', pattern='*.py')
+        ut.TextTestRunner().run(all_tests)
+
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.debug, threaded=True)
