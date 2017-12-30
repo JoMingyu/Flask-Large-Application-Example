d8981aa9f1b8770613ec52a170333864395aec58
JoMingyu <city7310@naver.com>
2017-12-30 20:27:00 +0900

    [UPDATE] Remove unnecessary CORS handling, argument parser

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,16 +1,10 @@
-from argparse import ArgumentParser
-
 from flask import Flask
-from flask_cors import CORS
 from flasgger import Swagger
 
 from app.docs import TEMPLATE
 from app.models import Mongo
 from app.views import ViewInjector
 from app.middleware import ErrorHandler, Logger
-
-cors = CORS()
-# To Swagger, or Support AJAX
 
 swagger = Swagger(template=TEMPLATE)
 # To Swagger UI
@@ -37,7 +31,6 @@ def create_app(config_name):
     app_ = Flask(__name__)
     app_.config.from_pyfile(config_name)
 
-    cors.init_app(app_)
     swagger.init_app(app_)
     db.init_app(app_)
     view.init_app(app_)
@@ -46,17 +39,11 @@ def create_app(config_name):
 
     return app_
 
-parser = ArgumentParser('Arguments to set configs for server')
-parser.add_argument('-c', '--config', required=True, help='Type of config(d : dev/p : production)')
-parser.add_argument('-p', '--port', help='Port which the server will operate', type=int)
-args = parser.parse_args()
+config = input('Type of config(d: dev/p: production) : ')
 
-assert args.config == 'd' or args.config == 'p'
+assert config == 'd' or config == 'p'
 
-if args.config == 'd':
+if config == 'd':
     app = create_app('../config/dev.py')
 else:
     app = create_app('../config/production.py')
-
-if args.port:
-    app.config['PORT'] = args.port
