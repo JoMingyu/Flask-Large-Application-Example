d3cad647591f31fed3897562f5cecfe087ca3664
JoMingyu <city7310@naver.com>
2018-01-27 21:04:14 +0900

    [UPDATE] Add json_response helper function

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -1,11 +1,21 @@
+import json
 import time
 
+from flask import Response
 from flask_restful import Resource
 
 
 class BaseResource(Resource):
     def __init__(self):
         self.now = time.strftime('%Y-%m-%d %H:%M:%S')
+
+    @classmethod
+    def json_response(cls, data, status_code=200):
+        return Response(
+            json.dumps(data, ensure_ascii=False),
+            status_code,
+            'application/json; charset=utf8'
+        )
 
 
 class ViewInjector(object):
