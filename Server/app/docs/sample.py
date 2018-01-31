c516239b4bba2677d716c2b6dfa14dca2229a553
JoMingyu <city7310@naver.com>
2018-01-31 14:50:38 +0900

    [ADD] Swagger example

---
+++ Server/app/docs/sample.py
@@ -0,0 +1,17 @@
+SAMPLE_GET = {
+    'tags': ['Some Tag'],
+    'description': '계정 삭제 후 새로운 UUID 생성',
+    'parameters': [
+    ],
+    'responses': {
+        '200': {
+            'description': 'Success',
+            'examples': {
+                'application/json': {
+                    'at': '2018-01-01 12:00:00',
+                    'msg': 'hello!'
+                }
+            }
+        }
+    }
+}
--- Server/app/views/sample.py
+++ Server/app/views/sample.py
@@ -1,6 +1,8 @@
-from flask_restful import Api
+from flasgger import swag_from
 from flask import Blueprint
+from flask_restful import Api
 
+from app.docs.sample import *
 from app.views import BaseResource
 
 api = Api(Blueprint('sample_api', __name__))
@@ -9,6 +11,7 @@ api.prefix = '/prefix'
 
 @api.resource('/sample')
 class Sample(BaseResource):
+    @swag_from(SAMPLE_GET)
     def get(self):
         return self.unicode_safe_json_response({
             'at': self.now,
