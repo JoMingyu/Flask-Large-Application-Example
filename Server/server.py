11b0e96e6aa482ac120292922227f9a207291c84
JoMingyu <city7310@naver.com>
2018-02-03 12:10:14 +0900

    [UPDATE] Add environment variable 'SECRET_KEY' not set warning

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,9 @@
+import os
+
 from app import app
 
 if __name__ == '__main__':
+    if 'SECRET_KEY' not in os.environ:
+        print('[WARN] SECRET KEY is not set in the environment variable.')
+
     app.run(host=app.config['HOST'], port=app.config['PORT'], debug=app.debug, threaded=True)
