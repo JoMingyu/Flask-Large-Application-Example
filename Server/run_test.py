ac89c5ae90c155de55976d7e09eab04b2ace287c
JoMingyu <city7310@naver.com>
2018-02-07 08:58:05 +0900

    [UPDATE] Add --testing argument to run_test.py

--- Server/run_test.py
+++ Server/run_test.py
@@ -1,5 +1,14 @@
+from argparse import ArgumentParser
 import unittest as ut
 
+from server import app
+
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--testing')
+    args = parser.parse_args()
+
+    app.testing = True if args.testing else False
+
     all_tests = ut.TestLoader().discover('tests', '*.py')
     ut.TextTestRunner().run(all_tests)
