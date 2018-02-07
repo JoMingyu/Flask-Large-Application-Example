e2f53ea792146d03caef32556410f5dde2d4deec
JoMingyu <city7310@naver.com>
2018-02-07 15:42:31 +0900

    [UPDATE] Remove argumentparser of run_test.py
    app.testing is always True when run_test.py runs __main__

--- Server/run_test.py
+++ Server/run_test.py
@@ -1,14 +1,9 @@
-from argparse import ArgumentParser
 import unittest as ut
 
 from server import app
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--testing')
-    args = parser.parse_args()
-
-    app.testing = True if args.testing else False
+    app.testing = True
 
     all_tests = ut.TestLoader().discover('tests', '*.py')
     ut.TextTestRunner().run(all_tests)
