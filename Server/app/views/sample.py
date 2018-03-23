bc5f801ea1f81c890f15beed52ee6e24fd488069
JoMingyu <city7310@naver.com>
2018-03-23 10:45:01 +0900

    UPDATE: Sample API

--- Server/app/views/sample.py
+++ Server/app/views/sample.py
@@ -1,9 +1,9 @@
 from flasgger import swag_from
-from flask import Blueprint
+from flask import Blueprint, request
 from flask_restful import Api
 
 from app.docs.sample import *
-from app.views import BaseResource
+from app.views import BaseResource, json_required
 
 api = Api(Blueprint('sample_api', __name__))
 api.prefix = '/prefix'
@@ -12,8 +12,6 @@ api.prefix = '/prefix'
 @api.resource('/sample')
 class Sample(BaseResource):
     @swag_from(SAMPLE_GET)
+    @json_required('name', 'age')
     def get(self):
-        return self.unicode_safe_json_dumps({
-            'at': self.now,
-            'msg': 'hello!'
-        })
+        return self.unicode_safe_json_dumps(request.json, 201)
