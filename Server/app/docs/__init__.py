e0b461099023ef1db987f8131544f1a079628218
JoMingyu <city7310@naver.com>
2018-04-17 11:20:10 +0900

    UPDATE: docs/__init__.py에 sample value들 추가

--- Server/app/docs/__init__.py
+++ Server/app/docs/__init__.py
@@ -1,3 +1,19 @@
+SAMPLE_OBJECT_IDS = [
+    '5acddc2bc2a93f68ce96f5c4',
+    '5acddc2bc2a93f68ce96f5c9',
+    '5acddc2bc2a93f68ce96f5ce'
+]
+SAMPLE_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' \
+                      'eyJmcmVzaCI6ZmFsc2UsImlkZW50aXR5IjoiYSIsInR5cGUiOiJhY2Nlc3MiLCJleHAiOjE1NDA1NT' \
+                      'c0NDYsImp0aSI6ImJiN2M3MjJmLTZkZjMtNDljYy1iZTk5LWRkMjMzNDU1NDRjZSIsIm5iZiI6MTUwOTA' \
+                      'yMTQ0NiwiaWF0IjoxNTA5MDIxNDQ2fQ.' \
+                      'wmytxSuQlH-KjhxO2EzrIioWHWgEnyiqWpRBwWuM15M'
+SAMPLE_REFRESH_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.' \
+                       'eyJleHAiOjE1MTM5MDQ3NjMsImlkZW50aXR5IjoibmlsIiwiZnJlc2giOmZhbHNlLCJqdGkiOiI1Y' \
+                       'zg1ZDAxNy1lYjIwLTRmYjgtYmVhYi1iYmYyZTQyY2NlYmYiLCJuYmYiOjE1MTM2NDU1NjMsInR5cGU' \
+                       'iOiJhY2Nlc3MiLCJpYXQiOjE1MTM2NDU1NjN9.' \
+                       '075C0_-b-oqSWc-jz7G35y00erRVntpcqN9uMIAnvfI'
+
 TEMPLATE = {
     'schemes': [
         'http'
