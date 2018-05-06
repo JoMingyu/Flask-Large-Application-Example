81cb0a689c728c4ca4b4deda55b6e9f75b396f08
JoMingyu <city7310@naver.com>
2018-05-06 17:59:45 +0900

    UPDATE: 샘플 API에 gzipped 데코레이터 추가

--- Server/app/views/sample.py
+++ Server/app/views/sample.py
@@ -3,7 +3,7 @@ from flask import Blueprint, request
 from flask_restful import Api
 
 from app.docs.sample import *
-from app.views import BaseResource, json_required
+from app.views import BaseResource, json_required, gzipped
 
 api = Api(Blueprint('/sample', __name__))
 api.prefix = '/prefix'
@@ -12,6 +12,7 @@ api.prefix = '/prefix'
 @api.resource('/sample')
 class Sample(BaseResource):
     @swag_from(SAMPLE_POST)
+    @gzipped
     @json_required('name', 'age')
     def post(self):
         return self.unicode_safe_json_dumps(request.json, 201)
