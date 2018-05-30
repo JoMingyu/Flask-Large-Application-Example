acb0a6e63140fc09ffca561fa60715da6861910f
JoMingyu <city7310@naver.com>
2018-05-30 17:20:14 +0900

    [UPDATE] InfluxDB 데이터베이스 미존재 시 create하는 구문 추가

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -33,11 +33,15 @@ def create_app(*config_cls):
     CORS().init_app(app_)
     JWTManager().init_app(app_)
     Swagger(template=app_.config['SWAGGER_TEMPLATE']).init_app(app_)
+    Router().init_app(app_)
 
     connect(**app_.config['MONGODB_SETTINGS'])
     app_.config['REDIS_CLIENT'] = Redis(**app_.config['REDIS_SETTINGS'])
     app_.config['INFLUXDB_CLIENT'] = InfluxDBClient(**app_.config['INFLUXDB_SETTINGS'])
 
-    Router().init_app(app_)
+    cfg = app_.config
+
+    if cfg['INFLUXDB_SETTINGS']['database'] not in cfg['INFLUXDB_CLIENT'].get_list_database():
+        cfg['INFLUXDB_CLIENT'].create_database(cfg['INFLUXDB_SETTINGS']['database'])
 
     return app_
