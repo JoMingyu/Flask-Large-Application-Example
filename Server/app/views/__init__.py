c5ffbfc4be76a9fce110af63b653596f46a68796
JoMingyu <city7310@naver.com>
2018-09-01 20:27:35 +0900

    [UPDATE] request_callback_decorators -> callback_decorators

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -5,13 +5,13 @@ from flask import Blueprint, Response
 from flask_restful import Resource
 from werkzeug.exceptions import HTTPException
 
-from app import errorhandlers, request_callback_decorators
+from app import errorhandlers, callback_decorators
 
 api_v1_blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 
 api_v1_blueprint.register_error_handler(HTTPException, errorhandlers.http_exception_handler)
 api_v1_blueprint.register_error_handler(Exception, errorhandlers.broad_exception_error_handler)
-api_v1_blueprint.after_request(request_callback_decorators.after_request)
+api_v1_blueprint.after_request(callback_decorators.after_request)
 
 
 class BaseResource(Resource):
