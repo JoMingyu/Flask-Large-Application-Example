c4466183c28fcbc89311fd1436faa989b77cbeae
JoMingyu <city7310@naver.com>
2018-11-03 01:03:03 +0900

    after_request에 'Date' 헤더 추가

--- Server/app/extensions.py
+++ Server/app/extensions.py
@@ -1,14 +1,9 @@
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from flask_mongoengine import MongoEngine
 from flask_validation import Validator
 from flasgger import Swagger
 
 cors = CORS()
 jwt = JWTManager()
-mongoengine = MongoEngine()
 validator = Validator()
 swagger = Swagger()
-
-# jwt.user_loader_callback_loader(...)
-# jwt.user_identity_loader(...)
--- Server/app/hooks/request_context.py
+++ Server/app/hooks/request_context.py
@@ -1,6 +1,10 @@
+from email.utils import formatdate
+
+
 def after_request(response):
     try:
         response.headers['X-Content-Type-Options'] = 'nosniff'
         response.headers['X-Frame-Options'] = 'deny'
+        response.headers['Date'] = formatdate(timeval=None, localtime=False, usegmt=True)
     finally:
         return response
