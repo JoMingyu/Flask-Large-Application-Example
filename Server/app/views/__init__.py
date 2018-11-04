06cbc3fbb5b9f50505f82a3039d5abc692d5f99c
PlanB <mingyu.planb@gmail.com>
2018-11-04 15:02:14 +0900

    불필요한 self.now 제거

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -1,14 +1,10 @@
 import json
-import time
 
 from flask import Response
 from flask_restful import Resource
 
 
 class BaseResource(Resource):
-    def __init__(self):
-        self.now = time.strftime('%Y-%m-%d %H:%M:%S')
-
     @classmethod
     def unicode_safe_json_dumps(cls, data, status_code=200, **kwargs) -> Response:
         return Response(
