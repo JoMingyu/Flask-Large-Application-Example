dff952d31596bced938b860ffbea5cc1ac11e730
JoMingyu <city7310@naver.com>
2018-11-11 18:28:45 +0900

    Update Server/wsgi.py

--- Server/wsgi.py
+++ Server/wsgi.py
@@ -2,11 +2,13 @@ import os
 
 from app import create_app
 from config import Config, RemoteDBConfig
+from app.misc.log import log
 
 application = create_app(Config, RemoteDBConfig)
 
 if __name__ == '__main__':
     if 'SECRET_KEY' not in os.environ:
-        print('[WARN] SECRET KEY is not set in the environment variable.')
+        log(message='SECRET KEY is not set in the environment variable.',
+            keyword='WARN')
 
     application.run()
