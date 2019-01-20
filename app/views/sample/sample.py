9b8a78b0074debb3144a08f451651e6c2f3a0205
JoMingyu <mingyu.planb@gmail.com>
2019-01-20 15:52:42 +0900

    BaseResource 추가

--- app/views/sample/sample.py
+++ app/views/sample/sample.py
@@ -1,10 +1,10 @@
 from flask import request
-from flask_restful import Resource
 
 from app.decorators.json_validator import validate_with_jsonschema
+from app.views.base import BaseResource
 
 
-class Sample(Resource):
+class Sample(BaseResource):
     @validate_with_jsonschema({
         'type': 'object',
         'required': ['age', 'name'],
