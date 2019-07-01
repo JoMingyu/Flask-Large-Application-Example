d0efdc22a0ceb2468c767656e202369917c1f7d7
PlanB <mingyu.planb@gmail.com>
2019-07-01 21:21:40 +0900

    config에 ENV, DEBUG 설정을 추가

--- config/app_config.py
+++ config/app_config.py
@@ -2,8 +2,12 @@ import os
 
 
 class LocalLevelConfig:
+    ENV = 'development'
+    DEBUG = True
     SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
 
 
 class ProductionLevelConfig:
+    ENV = 'production'
+    DEBUG = False
     SECRET_KEY = os.getenv('SECRET_KEY')
--- constants/local_run.py
+++ constants/local_run.py
@@ -1,6 +1,5 @@
 RUN_SETTING = {
     'host': 'localhost',
     'port': 5000,
-    'debug': True,
     'threaded': True
 }
